5c08b753878820ef02a9280a338ea2c48e3bb01b
Mustafa Azim <info@mustafaz.com>
2022-06-22 22:51:44 +0200

    Revert "Sezgin branch1"

--- src/views/questionView.js
+++ src/views/questionView.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// <<<<<<< Sezgin_Branch1
-// import { ANSWERS_LIST_ID } from '../constants.js';
-// import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
-// import { theScore } from '../data.js';
-// import { SCORE_ID } from '../constants.js';
-import { NUMBER_OF_ANSWERED_QUESTIONS } from '../constants.js';
-// import { quizData } from '../data.js';
-// import { SHOW_REPORT } from '../constants.js';
-// =======
 import { ANSWERS_LIST_ID , EXPLANATION_BUTTON_ID, 
   GIVEUP_QUESTION_BUTTON_ID,QUESTION_CONTAINER_ID,
   NEXT_QUESTION_BUTTON_ID,
@@ -18,7 +9,6 @@ import { ANSWERS_LIST_ID , EXPLANATION_BUTTON_ID,
   TIMER_ID,
 } from '../constants.js';
 
-// >>>>>>> Merge_Branch
 
 /**
  * Create a full question element
@@ -29,27 +19,6 @@ export const createQuestionElement = (question,score) => {
   element.id = QUESTION_CONTAINER_ID;
   // I use String.raw just to get fancy colors for the HTML in VS Code.
   element.innerHTML = String.raw`
-//<<<<<<< Sezgin_Branch1
-
-  <p id="${NUMBER_OF_ANSWERED_QUESTIONS}">Question number: ${quizData.currentQuestionIndex+1} / ${quizData.questions.length}</p>
-  
-//   <p id="${SCORE_ID}">Your score is: ${theScore[0]}</p>
-  
-//   <h1>${question}</h1>
-
-//     <ul id="${ANSWERS_LIST_ID}">
-//     </ul>
-
-//     <button id="${NEXT_QUESTION_BUTTON_ID}">
-//       Next question
-//     </button>
-
-//     <button id="${SHOW_REPORT}">
-//     Show Result Report
-//     </button>
-//   `
-//   ;
-// =======
     <div id=${INFO_CONTAINER}>
       <div>
         <p>Score: <span id=${SCORE_ID}>${score}</span></p>
@@ -71,7 +40,6 @@ export const createQuestionElement = (question,score) => {
       </button>
     </div>
   `;
-///>>>>>>> Merge_Branch
 
   return element;
 };
--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -9,15 +9,10 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
-//<<<<<<< Sezgin_Branch1
-    <h1>Welcome Class 38 Demo Presentation</h1>
-//=======
     <h6>The Frontiers' Quiz</h6>
     <br>
-//>>>>>>> Merge_Branch
     <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
     <h6>Class 38</h6>
   `;
   return element;
-  /*Demo*/
 };
